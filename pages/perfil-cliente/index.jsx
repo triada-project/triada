b10a57513ba8c9a9cd243ecdedaafc9f233489b9
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-03-25 21:37:10 -0600

    working in responsive, md done

--- pages/perfil-cliente/index.jsx
+++ pages/perfil-cliente/index.jsx
@@ -14,12 +14,12 @@ export default function PerfilCliente() {
   return (
     <>
       <MenuMobileClient />
-      <main className="  flex flex-col items-center m-auto sm:grid sm:grid-cols-12">
+      <main className="  flex flex-col items-center m-auto sm:grid sm:grid-cols-[245px_minmax(245px,_1fr)]  ">
         <AsideCliente />
-        <section className="sm:col-start-3 sm:col-span-10 sm:grid sm:grid-cols-10 sm:h-screen">
-          <div className=" sm:ml-16 sm:col-start-1 sm:col-span-4">
+        <section className=" flex flex-col items-center sm:col-start-2 sm:col-span-1 sm:h-screen lg:flex-row lg:items-start">
+          <div className=" flex flex-col items-center lg:items-start lg:ml-[72px]">
             <h1
-              className={`${josefine.className} text-black text-xl font-semibold mt-10 sm:text-[28px]`}
+              className={`${josefine.className}  text-black text-xl font-semibold mt-10 sm:text-[28px] lg:mt-[72px] `}
             >
               Mi Perfil
             </h1>
