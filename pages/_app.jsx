abc25ff3c032e46bb60d802f62cb8ef598f87c9e
oswaldo-herrera <oswaldo.herrera@s4learning.com>
2024-03-27 19:13:10 -0600

    modalsClient

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import NavBarPrueba from "@/components/NavbarPrueba";
+import ModalCliente from "@/components/ModalCliente";
+import ModalClientePendiente from "@/components/ModalClientePendiente";
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
       <NavBarPrueba />
+      <ModalCliente></ModalCliente>
+      <ModalClientePendiente></ModalClientePendiente>
       {/* <Component {...pageProps} /> */}
     </NextUIProvider>
   );
