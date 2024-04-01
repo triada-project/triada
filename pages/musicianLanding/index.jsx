23de2cd8c4f3a58e388b54ae0c6367693f5643f0
Oswaldo Herrera <oswaldo.herrera.mx@gmail.com>
2024-03-31 22:42:30 -0600

    Client modal (#13)
    * first commit
    
    * first commit
    
    * musicianObject corregido, avance musicianLanding
    
    * ranking and checks
    
    * modalsClient
    
    * domy calendar
    
    * merge develop_01
    
    ---------
    
    Co-authored-by: Armando García <art.gamir@gmail.com>
    Co-authored-by: Armando García <95318288+ArtGamir@users.noreply.github.com>
    Co-authored-by: oswaldo-herrera <oswaldo.herrera@s4learning.com>

---
+++ pages/musicianLanding/index.jsx
@@ -0,0 +1,213 @@
+import React from "react";
+import dataMusician from "../../objects/musicianObject.json";
+import { Avatar, AvatarIcon, Chip } from "@nextui-org/react";
+import { Josefin_Sans, Lato, Pacifico } from "next/font/google";
+import check from "../../public/assets/svg/check.svg";
+import Image from "next/image";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import CalendarUI from "../../components/CalendarUI";
+
+const josefin = Josefin_Sans({
+  weight: ["300", "400", "600", "700"],
+  subsets: ["latin"],
+});
+const lato = Lato({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
+const pacifico = Pacifico({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export default function musicianDetail() {
+  const { users } = dataMusician;
+  console.log(users.musicalGenere);
+
+  const musicalGeneres = users.musicalGenere;
+  const eventType = users.eventType;
+
+  return (
+    <>
+      <div className="ml-5 mt-5 mr-5">
+        <div className=" ">
+          <Avatar
+            src={users.profilePicture[0].url}
+            alt="profile picture"
+            className="w-80 h-60 rounded-lg shadow-xl text-large"
+          />
+        </div>
+        <div className="mt-5">
+          <h1 className="{`${josefin.classname} text-black text-3xl font-semibold mt-5 sm:text-[28px]">
+            {users.name}
+          </h1>
+          <h3 className="text-[455A64]">
+            {users.location[0].city}, {users.location[0].state}
+          </h3>
+          <div className="flex items-centerflex items-center">
+            <Rating style={{ maxWidth: 100 }} value={users.ranking} readOnly />
+            <div className="ml-2">{users.ranking}</div>
+          </div>
+          <div>
+            {musicalGeneres.map((nombre, index) => (
+              // Usamos map para iterar sobre la lista de nombres y renderizar cada uno como un elemento <li>
+              <Chip
+                classNames={{
+                  base: "bg-[#081540] h-[32px] mt-2",
+                  content: "text-[#29FEFD] ",
+                }}
+              >
+                {nombre}
+              </Chip>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Descripción
+          </h2>
+          <p className="{`${josefin.classname} text-[#455A64]">
+            {users.description}
+          </p>
+        </div>
+        <div>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Eventos en los que toca
+          </h2>
+          {eventType.map((nombre, index) => (
+            // Usamos map para iterar sobre la lista de nombres y renderizar cada uno como un elemento <li>
+            <Chip
+              classNames={{
+                base: "bg-[#081540] h-[32px] mt-2",
+                content: "text-[#29FEFD] ",
+              }}
+            >
+              {nombre}
+            </Chip>
+          ))}
+        </div>
+        <div>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Videos
+          </h2>
+          <div>
+            <p>Carousel</p>
+          </div>
+        </div>
+        <div>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Fotos
+          </h2>
+          <div>
+            <p>Carousel</p>
+          </div>
+        </div>
+        <div>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Requerimientos
+          </h2>
+          <div>
+            <ul>
+              <div className="flex items-center">
+                <Image src={check} alt="check" width={20} height={20} />
+                <li className="ml-2">Piano de 88 teclas afinado</li>
+              </div>
+              <div className="flex items-center">
+                <Image src={check} alt="check" width={20} height={20} />
+                <li className="ml-2">Camerino con iluminación y espejo</li>
+              </div>
+              <div className="flex items-center">
+                <Image src={check} alt="check" width={20} height={20} />
+                <li className="ml-2">Catering ligero</li>
+              </div>
+              <div className="flex items-center">
+                <Image src={check} alt="check" width={20} height={20} />
+                <li className="ml-2">Agua</li>
+              </div>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+              Tiempo máximo de concierto
+            </h2>
+            <Chip
+              classNames={{
+                base: "bg-[#081540] h-[32px] mt-2",
+                content: "text-[#29FEFD]",
+              }}
+            >
+              {users.maxHours} horas
+            </Chip>
+          </div>
+          <div>
+            <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+              Costo por hora
+            </h2>
+            <Chip
+              classNames={{
+                base: "bg-[#081540] h-[32px] mt-2",
+                content: "text-[#29FEFD]",
+              }}
+            >
+              {users.eventFee}
+            </Chip>
+          </div>
+        </div>
+        <div className="border-2 rounded-lg mt-5">
+          <div className="ml-5 mr-5 ">
+            <h1 className="{`${roboto.classname} text-[#312971] text-2xl text-center font-bold mt-5 sm:text-[30px]">
+              Inicia la reservación para tu evento aquí
+            </h1>
+            <div>
+              <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                Elige una fecha
+              </h2>
+              <div className="border-2 rounded-lg mt-5">
+                <CalendarUI />
+              </div>
+              <div>
+                <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                  Elige el horario
+                </h2>
+                <p>(!)Blue text</p>
+                <div>
+                  <p>RHF hora inicio hora fin</p>
+                </div>
+                <span>
+                  <p>Horas contratadas</p>
+                </span>
+                <span>
+                  <p>suma de horas ingresadas</p>
+                </span>
+                <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                  Detalles del evento
+                </h2>
+                <div>
+                  <p>RHF hora inicio hora fin</p>
+                </div>
+                <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                  Resumen
+                </h2>
+                <span>
+                  <p>Horas contratadas</p>
+                </span>
+                <span>
+                  <p>suma de horas ingresadas</p>
+                </span>
+                <span>
+                  <p>Total de reservación</p>
+                </span>
+                <span>
+                  <p>multiplicación de horas ingresadas por costo por hora</p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
