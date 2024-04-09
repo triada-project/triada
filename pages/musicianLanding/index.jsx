731a67397e5691c21a5e68efaa4b4a2f81058318
Armando García <95318288+ArtGamir@users.noreply.github.com>
2024-04-08 23:04:08 -0600

    Musician detail UI ok (#25)
    * first commit
    
    * first commit
    
    * musicianObject corregido, avance musicianLanding
    
    * ranking and checks
    
    * domy calendar
    
    * carousels
    
    * components
    
    * repertorio y opiniones
    
    * responsive
    
    * chip gap
    
    * Delete .vscode/launch.json
    
    * footer
    
    * chips repertory
    
    * calendar
    
    * acamodo de dom
    
    * footer
    
    * carousel navigation
    
    * carousels OK
    
    * superior arangement
    
    * UI ready
    
    * merge UI ready
    
    * ok
    
    * max width
    
    * main center
    
    ---------
    
    Co-authored-by: Rodolfo Ramírez <126934315+RudyRmz@users.noreply.github.com>

--- pages/musicianLanding/index.jsx
+++ pages/musicianLanding/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Josefin_Sans, Lato } from "next/font/google";
-import { Josefin_Sans, Lato } from "next/font/google";
 import { Avatar, AvatarIcon, Chip } from "@nextui-org/react";
 import NavBar from "@/components/Navbar";
 import dataMusician from "../../objects/musicianObject.json";
@@ -11,7 +10,7 @@ import CarouselFotos from "../../components/musicianLanding/CarouselFotos";
 import AsideLeft from "@/components/musicianLanding/AsideLeft";
 import MainFooter from "../../components/mainFooter/MainFooter";
 import Ranking from "@/components/Ranking/Ranking";
-import EventInput from "@/components/musicianLanding/EventInput";
+import EventForm from "@/components/musicianLanding/EventForm";
 
 
 const josefin = Josefin_Sans({
@@ -33,80 +32,86 @@ export default function musicianDetail() {
 
   return (
     <>
-      <NavBar />
-      <div className="lg:ml-[80px] lg:mr-[80px] ">
-        <div className="grid grid-cols-1 sm:grid-cols-4 sm:grid-rows-9 grid-flow-col text-white  ">
-          <div className="p-5 col-start-1 flex justify-center ">
-            <Avatar
-              src={users.profilePicture[0].url}
-              alt="profile picture"
-              className="w-80 h-60 rounded-lg shadow-xl  "
-            />
-          </div>
-          <main className="col-start-1 sm:col-start-2 sm:col-span-3 row-span-9  p-5 ">
-            <div className="">
-              <h1 className="{`${josefin.classname} text-black text-3xl font-semibold sm:mt-0 sm:text-[28px]">
-                {users.name}
-              </h1>
-              <h3 className="text-[#455A64]">
-                {users.location[0].city}, {users.location[0].state}
-              </h3>
-              <div className="flex items-centerflex items-center">
-                <Ranking />
-              </div>
-              <div className="gap gap-1 flex flex-nowrap">
-                {musicalGeneres.map((nombre, index) => (
-                  <Chip
-                    classNames={{
-                      base: "bg-[#081540] h-[32px] mt-2",
-                      content: "text-[#29FEFD] ",
-                    }}
-                  >
-                    {nombre}
-                  </Chip>
-                ))}
-              </div>
+      <main className="max-w-[1440px] m-auto">
+        <NavBar />
+        <div className="lg:ml-[80px] lg:mr-[80px] ">
+          <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-4 md:grid-rows-9 grid-flow-col  ">
+            <div className="p-5 col-start-1 sm:col-span-2 md:col-span-1 flex justify-center ">
+              <Avatar
+                src={users.profilePicture[0].url}
+                alt="profile picture"
+                className="w-80 h-60 rounded-lg shadow-xl  "
+              />
             </div>
-            <div>
-              <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                Descripción
-              </h2>
-              <p className="{`${josefin.classname} text-[#455A64]">
-                {users.description}
-              </p>
-            </div>
-            <div>
-              <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                Eventos en los que toca
-              </h2>
-              <div className="gap gap-1 flex flex-nowrap">
-                {eventType.map((nombre, index) => (
-                  <Chip
-                    classNames={{
-                      base: "bg-[#081540] h-[32px] mt-2",
-                      content: "text-[#29FEFD] ",
-                    }}
-                  >
-                    {nombre}
-                  </Chip>
-                ))}
+
+            <main className="col-start-1 sm:col-start-3 sm:col-span-4 md:col-start-2 md:col-span-3 row-span-9  p-5 ">
+              <div className="">
+                <h1
+                  className={`${josefin.classname} text-black text-3xl font-semibold sm:mt-0 sm:text-[28px]`}
+                >
+                  {users.name}
+                </h1>
+                <h3 className="text-[#455A64]">
+                  {users.location[0].city}, {users.location[0].state}
+                </h3>
+                <div className="flex items-centerflex items-center">
+                  <Ranking />
+                </div>
+                <div className="gap gap-1 flex flex-nowrap">
+                  {musicalGeneres.map((nombre, index) => (
+                    <Chip
+                      classNames={{
+                        base: "bg-[#081540] h-[32px] mt-2",
+                        content: "text-[#29FEFD] ",
+                      }}
+                    >
+                      {nombre}
+                    </Chip>
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="md:flex md:flex-row md:gap-4">
-              <span>
+              <div>
                 <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                  Videos
+                  Descripción
                 </h2>
-                <CarouselVideos />
-              </span>
-              <span>
+                <p className="{`${josefin.classname} text-[#455A64]">
+                  {users.description}
+                </p>
+              </div>
+              <div>
                 <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                  Fotos
+                  Eventos en los que toca
                 </h2>
-                <CarouselFotos />
-              </span>
-            </div>
-            <div className="md:flex md:flex-row md:gap-56">
+                <div className="gap gap-1 flex flex-nowrap">
+                  {eventType.map((nombre, index) => (
+                    <Chip
+                      classNames={{
+                        base: "bg-[#081540] h-[32px] mt-2",
+                        content: "text-[#29FEFD] ",
+                      }}
+                    >
+                      {nombre}
+                    </Chip>
+                  ))}
+                </div>
+              </div>
+              <div className="md:flex md:flex-row md:gap-4">
+                <span>
+                  <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                    Videos
+                  </h2>
+                  <CarouselVideos />
+                </span>
+                <span>
+                  <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+                    Fotos
+                  </h2>
+                  <CarouselFotos />
+                </span>
+              </div>
+              {
+                //<div className="md:flex md:flex-row md:gap-56">
+              }
               <div>
                 <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
                   Requerimientos
@@ -162,20 +167,24 @@ export default function musicianDetail() {
                   </Chip>
                 </div>
               </div>
+              {
+                //</div>
+              }
+              <div className="mt-5 shadow-xl">
+                <EventForm />
+              </div>
+            </main>
+            <div className="col-start-1 sm:col-span-2 md:col-span-1 p-5 sm:row-start-2 sm:row-span-9 ">
+              <AsideLeft />
             </div>
-            <EventInput />
-          </main>
-          <div className="col-start-1 p-5 sm:row-start-2 sm:row-span-9 ">
-            <AsideLeft />
           </div>
         </div>
-      </div>
-      <div className="">
-        <MainFooter />
-      </div>
-      {
-        //</div>
-      }
+        <div className="">
+          <div className="">
+            <MainFooter />
+          </div>
+        </div>
+      </main>
     </>
   );
 }
--- pages/perfil-musico/galeria.jsx
+++ pages/perfil-musico/galeria.jsx
@@ -1,6 +1,12 @@
 import MenuMobileMusician from "@/components/profile-musician/MenuMobileMusician.jsx";
 import AsideMusico from "@/components/profile-musician/AsideMusico.jsx";
 import { Josefin_Sans, Lato } from "next/font/google";
+import { useForm } from "react-hook-form";
+import { Button } from "@nextui-org/react";
+import { Input } from "@nextui-org/react";
+import { Image } from "next/image";
+import addImage from "../../public/assets/svg/addImage.svg";
+import ButtonPink from "../../components/musicianLanding/ButtonPink";
 
 const josefine = Josefin_Sans({
   weight: ["300", "400", "600", "700"],
@@ -9,6 +15,13 @@ const josefine = Josefin_Sans({
 const lato = Lato({ weight: ["300", "400", "700"], subsets: ["latin"] });
 
 export default function PerfilMusico() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
   return (
     <>
       <MenuMobileMusician page="galeria" role="musico" />
@@ -20,6 +33,36 @@ export default function PerfilMusico() {
           >
             Galería
           </h1>
+          <p className={`${lato.className} text-[#455A64]`}>
+            Sube fotos y videos de tus presentaciones para que el público
+            conozca tu trabajo.
+          </p>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Fotos
+          </h2>
+          <Button>addImage</Button>
+          <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+            Videos
+          </h2>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex gap-2 items-center w-full mt-2 "
+          >
+            <Input
+              isRequired
+              variant="bordered"
+              radius="sm"
+              label="URL"
+              {...register("URL")}
+              className=""
+            />
+            <ButtonPink
+              width="w-[140px] lg:w-[30rem]"
+              text="Agregar"
+              type="submit"
+              className=""
+            />
+          </form>
         </section>
       </main>
     </>
