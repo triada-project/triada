e93dba61313acc630d84525b1a0f9bb28d29e49e
Armando García <art.gamir@gmail.com>
2024-04-08 23:46:00 -0600

    footer correction

--- pages/musicianLanding/index.jsx
+++ pages/musicianLanding/index.jsx
@@ -21,7 +21,6 @@ const lato = Lato({
   subsets: ["latin"],
 });
 
-
 export default function musicianDetail() {
   const { users } = dataMusician;
   console.log(users.musicalGenere);
@@ -31,10 +30,10 @@ export default function musicianDetail() {
 
   return (
     <>
-      <main className="max-w-[1440px] m-auto">
+      <main className="max-w-[1440px] m-auto bg-white">
         <NavBar />
         <div className="lg:ml-[80px] lg:mr-[80px] ">
-          <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-4 md:grid-rows-9 grid-flow-col  ">
+          <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-4 md:grid-rows-7 grid-flow-col  ">
             <div className="p-5 col-start-1 sm:col-span-2 md:col-span-1 flex justify-center ">
               <Avatar
                 src={users.profilePicture[0].url}
@@ -43,87 +42,7 @@ export default function musicianDetail() {
               />
             </div>
 
-            <main className="col-start-1 sm:col-start-3 sm:col-span-4 md:col-start-2 md:col-span-3 row-span-9  p-5 ">
-              <div className="">
-                <h1
-                  className={`${josefin.classname} text-black text-3xl font-semibold sm:mt-0 sm:text-[28px]`}
-                >
-                  {users.name}
-                </h1>
-                <h3 className="text-[#455A64]">
-                  {users.location[0].city}, {users.location[0].state}
-                </h3>
-                <div className="flex items-centerflex items-center">
-                  <Ranking />
-                </div>
-                <div className="gap gap-1 flex flex-nowrap">
-                  {musicalGeneres.map((nombre, index) => (
-                    <Chip
-                      classNames={{
-                        base: "bg-[#081540] h-[32px] mt-2",
-                        content: "text-[#29FEFD] ",
-                      }}
-                    >
-                      {nombre}
-                    </Chip>
-                  ))}
-                </div>
-              </div>
-              <div>
-                <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                  Descripción
-                </h2>
-                <p className="{`${josefin.classname} text-[#455A64]">
-                  {users.description}
-                </p>
-              </div>
-              <div>
-                <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                  Eventos en los que toca
-                </h2>
-                <div className="gap gap-1 flex flex-nowrap">
-                  {eventType.map((nombre, index) => (
-                    <Chip
-                      classNames={{
-                        base: "bg-[#081540] h-[32px] mt-2",
-                        content: "text-[#29FEFD] ",
-                      }}
-                    >
-                      {nombre}
-                    </Chip>
-                  ))}
-                </div>
-              </div>
-              <div className="md:flex md:flex-row md:gap-4">
-                <span>
-                  <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                    Videos
-                  </h2>
-                  <CarouselVideos />
-                </span>
-                <span>
-                  <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-                    Fotos
-                  </h2>
-                  <CarouselFotos />
-                </span>
-              </div>
-              {
-                //<div className="md:flex md:flex-row md:gap-56">
-              }
-      <main className="max-w-[1440px] m-auto">
-        <NavBar />
-        <div className="lg:ml-[80px] lg:mr-[80px] ">
-          <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-4 md:grid-rows-9 grid-flow-col  ">
-            <div className="p-5 col-start-1 sm:col-span-2 md:col-span-1 flex justify-center ">
-              <Avatar
-                src={users.profilePicture[0].url}
-                alt="profile picture"
-                className="w-80 h-60 rounded-lg shadow-xl  "
-              />
-            </div>
-
-            <main className="col-start-1 sm:col-start-3 sm:col-span-4 md:col-start-2 md:col-span-3 row-span-9  p-5 ">
+            <main className="col-start-1 sm:col-start-3 sm:col-span-4 md:col-start-2 md:col-span-3 row-span-7  p-5 ">
               <div className="">
                 <h1
                   className={`${josefin.classname} text-black text-3xl font-semibold sm:mt-0 sm:text-[28px]`}
@@ -258,10 +177,9 @@ export default function musicianDetail() {
             </div>
           </div>
         </div>
+
         <div className="">
-          <div className="">
-            <MainFooter />
-          </div>
+          <MainFooter />
         </div>
       </main>
     </>
