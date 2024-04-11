1fa4178f33e8258b6dac8ad26a7d1dd5683a5464
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-04-10 18:44:47 -0600

    footer tablet done

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -6,41 +6,47 @@ const lato = Lato({ weight: ["300", "400", "700"], subsets: ["latin"] });
 export default function FooterMain() {
   return (
     <footer
-      className=" w-full bg-cover bg-center flex flex-col items-center pt-14"
+      className=" w-full bg-cover bg-center flex flex-col items-center pt-14 sm:pt-36 "
       style={{
         backgroundImage: `url('/assets/svg/footer-svg.svg')`,
         width: "100%",
         height: "100%",
       }}
     >
-      <section className=" w-[328px] h-[464px] flex flex-col items-center ">
+      <section className=" w-[328px] h-[464px] flex flex-col justify-center items-center sm:h-72 sm:w-[630px]">
         <div
-          className={` w-[328px] ${lato.className} flex flex-col gap-7 items-center text-white border-b-1 pb-7`}
+          className={` w-full ${lato.className} flex flex-col gap-7 items-center text-white border-b-1 pb-7 sm:flex-row sm:justify-center sm:gap-8 `}
         >
           <Image src={"/assets/svg/triada-logo.svg"} width={74} height={38} />
           <p>Nosotros</p>
           <p>Músicos</p>
           <p>Preguntas frecuentes</p>
         </div>
-        <div
-          className={` mt-7 w-[328px] ${lato.className} flex flex-col gap-7 items-center text-white `}
-        >
-          <p>© 2024</p>
-          <p>Aviso de privacidad</p>
-          <p>Términos y condiciones</p>
-        </div>
-        <div className=" mt-7 flex gap-6">
-          <Image
-            src={"/assets/svg/logo-instagram.svg"}
-            width={20}
-            height={20}
-          />
-          <Image src={"/assets/svg/logo-facebook.svg"} width={20} height={20} />
-          <Image
-            src={"/assets/svg/logo-x-twitter.svg"}
-            width={20}
-            height={20}
-          />
+        <div className=" flex flex-col items-center sm:flex-row sm:gap-12">
+          <div
+            className={` mt-7 w-full ${lato.className} flex flex-col gap-7 items-center text-white sm:flex-row `}
+          >
+            <p>© 2024</p>
+            <p>Aviso de privacidad</p>
+            <p>Términos y condiciones</p>
+          </div>
+          <div className=" mt-7 flex gap-6">
+            <Image
+              src={"/assets/svg/logo-instagram.svg"}
+              width={20}
+              height={20}
+            />
+            <Image
+              src={"/assets/svg/logo-facebook.svg"}
+              width={20}
+              height={20}
+            />
+            <Image
+              src={"/assets/svg/logo-x-twitter.svg"}
+              width={20}
+              height={20}
+            />
+          </div>
         </div>
       </section>
     </footer>
