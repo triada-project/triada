5319bdb99e6ad6ef4699605809ad281a66f4ea7c
Alejandrojunio29 <127553614+Alejandrojunio29@users.noreply.github.com>
2024-04-16 12:05:26 -0600

    I added the register modal  (#39)
    * made some changes to the navbar
    
    * added the components of buttons & anchors to the navbar
    
    * added the main section of the image, and fixed some details in the navbar
    
    * added the components card & the card´s section, fix some svg because they were wrong
    
    * added the responsive config to the cards´s section
    
    * fixed the mobile section of the cards´s section
    
    * added the about us Section
    
    * finish the us section
    
    * finish all the about us section & responsive also
    
    * added the genres gradient bar
    
    * fix the gradient bar
    
    * just because
    
    * added the musician catalog responsive
    
    * added the musician cards
    
    * made some changes
    
    * fix the issues of mobile first
    
    * fix the tablet´s breakpoints
    
    * fix the catalog of musicians cards to tablet view
    
    * ready the tablet view, just to fix the footer
    
    * finish the desktop view
    
    * added the footer
    
    * added the slider cards to mobile & buttons to toggle menu of navbar
    
    * added the auto play slider to the gradient bar of genres
    
    * added the function scroll to top to gradient button
    
    * added the modal of log in
    
    * added the register modal
    
    * i erase the layout page cause i didnt use it anymore
    
    ---------
    
    Co-authored-by: Rodolfo Ramírez <126934315+RudyRmz@users.noreply.github.com>

---
+++ components/HomeComponents/NavbarComponents/RegisterForm.jsx
@@ -0,0 +1,132 @@
+import { useForm } from "react-hook-form";
+import { Josefin_Sans, Lato } from "next/font/google";
+import ClientOrMusician from "./ClientOrMusician";
+
+const josefine = Josefin_Sans({
+  weight: ["300", "400", "600", "700"],
+  subsets: ["latin"],
+});
+const lato = Lato({ weight: ["300", "400", "700"], subsets: ["latin"] });
+
+export default function RegisterForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex ">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col gap-[20px] w-[331px] lg:w-[387px]"
+      >
+        <div className="flex flex-col  ">
+          <div
+            className={`flex flex-col justify-start items-center text-[24px]   font-bold lg:text-[32px] lg:items-start ${josefine.className} `}
+          >
+            Crea una cuenta
+          </div>
+          <ClientOrMusician />
+          <label
+            className={`pb-[8px] font-bold text-[16px] ${josefine.className}`}
+          >
+            Correo Electrónico
+          </label>
+          <input
+            id="email"
+            type="text"
+            className="text-black w-[310px] sm:w-[365px] pl-2 h-[40px] rounded-xl"
+            {...register("email", {
+              required: true,
+              pattern: /^[^\s]+@[^\s@]+\.[^\s@]+$/i,
+            })}
+          />
+          {errors.email?.type === "required" && (
+            <p> El campo correo Electronico es requerido</p>
+          )}
+          {errors.email?.type === "pattern" && (
+            <p> El formato del correo electronico es incorrecto</p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <label
+            className={`pb-[8px] font-bold text-[16px] ${josefine.className}`}
+          >
+            Contraseña
+          </label>
+          <input
+            id="password1"
+            type="password"
+            className="text-black  pl-2 w-[310px] sm:w-[365px] h-[40px] rounded-xl"
+            {...register("password", {
+              required: true,
+              pattern: /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/,
+              minLength: 6,
+              maxLength: 12,
+            })}
+          />
+          {errors.password?.type === "required" && (
+            <p> El campo contraseña es requerido</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p>
+              {" "}
+              La contraseña requiere al menos un dígito, una minúscula y una
+              mayúscula
+            </p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p> La contraseña requiere al menos 6 caracteres</p>
+          )}
+          {errors.password?.type === "maxLength" && (
+            <p> La contraseña solo puede incluir hasta 12 caracteres</p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <label
+            className={`pb-[8px] font-bold text-[16px] ${josefine.className}`}
+          >
+            Confirmar contraseña
+          </label>
+          <input
+            id="password2"
+            type="password"
+            className="text-black  pl-2 w-[310px] sm:w-[365px] h-[40px] rounded-xl"
+            {...register("password2", {
+              required: true,
+              pattern: /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/,
+              minLength: 6,
+              maxLength: 12,
+            })}
+          />
+          {errors.password2?.type === "required" && (
+            <p> El campo confirmar contraseña es requerido</p>
+          )}
+          {errors.password?.type === "pattern" && (
+            <p>
+              {" "}
+              La contraseña requiere al menos un dígito, una minúscula y una
+              mayúscula
+            </p>
+          )}
+          {errors.password?.type === "minLength" && (
+            <p> La contraseña requiere al menos 6 caracteres</p>
+          )}
+          {errors.password?.type === "maxLength" && (
+            <p> La contraseña solo puede incluir hasta 12 caracteres</p>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          value="CREAR PERFIL"
+          className={`bg-[#EE0075] rounded-xl font-semibold text-[18px] w-[310px] sm:w-[365px] h-[42px] ${josefine.className} `}
+        />
+      </form>
+    </div>
+  );
+}
