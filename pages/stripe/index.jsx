dcb466265899cacbd0584ae907a6d92b82095d7d
Oswaldo Herrera <oswaldo.herrera.mx@gmail.com>
2024-04-15 21:03:15 -0600

    Client modal (#37)
    * first commit
    
    * first commit
    
    * musicianObject corregido, avance musicianLanding
    
    * ranking and checks
    
    * modalsClient
    
    * domy calendar
    
    * merge develop_01
    
    * cards-respon
    
    * last_changes
    
    * last_changes_01
    
    * connect modal-table-client
    
    * stripe
    
    * stripe_01
    
    ---------
    
    Co-authored-by: Armando García <art.gamir@gmail.com>
    Co-authored-by: Armando García <95318288+ArtGamir@users.noreply.github.com>
    Co-authored-by: oswaldo-herrera <oswaldo.herrera@s4learning.com>

---
+++ pages/stripe/index.jsx
@@ -0,0 +1,15 @@
+import StripePayments from "@/components/Stripe/StripePayments"
+import InjectedCheckoutForm from "@/components/Stripe/InjectedCheckoutForm"
+
+
+
+export default function Index() {
+    return (
+        <>
+      
+            <StripePayments></StripePayments>
+            {/* <InjectedCheckoutForm></InjectedCheckoutForm> */}
+            
+        </>
+    )
+}
