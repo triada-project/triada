4078c85a7e1d087535d4b08dbc79dbd28a53dd17
Armando García <95318288+ArtGamir@users.noreply.github.com>
2024-04-16 20:16:03 -0600

    Galery (#41)
    * first commit
    
    * first commit
    
    * musicianObject corregido, avance musicianLanding
    
    * ranking and checks
    
    * domy calendar
    
    * carousels
    
    * components
    
    * repertorio y opiniones
    
    * responsive
    
    * chip gap
    
    * Delete .vscode/launch.json
    
    * footer
    
    * chips repertory
    
    * calendar
    
    * acamodo de dom
    
    * footer
    
    * carousel navigation
    
    * carousels OK
    
    * superior arangement
    
    * UI ready
    
    * merge UI ready
    
    * ok
    
    * first commit
    
    * start
    
    * galery components
    
    * add-photo button
    
    * Gallery with media
    
    * Save
    
    ---------
    
    Co-authored-by: Rodolfo Ramírez <126934315+RudyRmz@users.noreply.github.com>

--- components/galeria/GaleryPhotos.jsx
+++ components/galeria/GaleryPhotos.jsx
@@ -1,4 +1,16 @@
-export default function GaleryPhotos() {
-    return (
-       section
-    )
+import AddPhoto from "./AddPhoto";
+import CardPics from "./CardPics";
+
+export default function GalleryPhotos() {
+  return (
+    <>
+      <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+        Fotos
+      </h2>
+      <div className="flex gap-4">
+        <AddPhoto />
+        <CardPics />
+      </div>
+    </>
+  );
+}
