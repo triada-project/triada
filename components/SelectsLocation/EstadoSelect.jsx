63cfb47f4752fd51aa5009563402bcc401344ea9
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-04-18 13:16:15 -0600

    Zustand was implemented for state management. The stores folder was created, within it the store selectedStateStore was created, and it was implemented in the EstadoSelect and LocalidadSelect components

--- components/SelectsLocation/EstadoSelect.jsx
+++ components/SelectsLocation/EstadoSelect.jsx
@@ -1,28 +1,40 @@
 import { Select, SelectSection, SelectItem } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
 import estados from "../../data/estados.json";
+import useSelectedStateStore from "@/stores/selectedStateStore";
 
 //console.log(estados);
 
 export default function EstadoSelect() {
+  const setSelectedState = useSelectedStateStore(
+    (state) => state.setSelectedState
+  );
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  const handleEstadoChange = (selectedEstado) => {
+    setSelectedState(selectedEstado);
+  };
+
   return (
     <Select
       label="Estado de residencia"
       isRequired
-      //autoFocus={true}
       variant="bordered"
       radius="sm"
-      className={`w-[328px] h-14 lg:w-[30rem]`}
-      //errorMessage={!errors.estado ? "" : "Debes elegir un Estado"}
+      className="w-[328px] h-14 lg:w-[30rem]"
       {...register("estado", { required: true })}
     >
       {estados.map((estado) => (
-        <SelectItem key={estado.clave}>{estado.nombre}</SelectItem>
+        <SelectItem
+          key={estado.clave}
+          onClick={() => handleEstadoChange(estado.nombre)}
+        >
+          {estado.nombre}
+        </SelectItem>
       ))}
     </Select>
   );
--- components/SelectsLocation/LocalidadSelect.jsx
+++ components/SelectsLocation/LocalidadSelect.jsx
@@ -1,25 +1,32 @@
-import { Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { Select, SelectItem } from "@nextui-org/react";
 import { useForm } from "react-hook-form";
+import estadosMunicipios from "../../data/estados-municipios.json";
+import useSelectedStateStore from "@/stores/selectedStateStore";
 
 export default function LocalidadSelect() {
+  const selectedState = useSelectedStateStore((state) => state.selectedState);
+  console.log(selectedState);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
+  // Filtrar las localidades según el estado seleccionado
+  const localidades = estadosMunicipios[selectedState] || [];
+
   return (
     <Select
       label="Localidad"
       isRequired
       variant="bordered"
       radius="sm"
-      className=" w-[328px] h-14 lg:w-[30rem]"
+      className="w-[328px] h-14 lg:w-[30rem]"
       {...register("localidad")}
     >
-      {}
-      <SelectItem key={"Monterrey"}>Monterrey</SelectItem>
-      <SelectItem key={"Santa Catarina"}>Santa Catarina</SelectItem>
-      <SelectItem key={"San Pedro"}>San Pedro</SelectItem>
+      {localidades.map((localidad, index) => (
+        <SelectItem key={index}>{localidad}</SelectItem>
+      ))}
     </Select>
   );
 }
