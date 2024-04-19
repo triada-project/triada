94cb0bbd064a59f35ce26720e6625a3f34b4ff8c
Armando García <art.gamir@gmail.com>
2024-04-18 20:01:39 -0600

    Videos

--- components/musicianLanding/CarouselVideos.jsx
+++ components/musicianLanding/CarouselVideos.jsx
@@ -1,68 +1,45 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
-// import stylesheet if you're not already using CSS @import
 import "react-image-gallery/styles/css/image-gallery.css";
 import YouTube from "react-youtube";
 
 export default function CarouselVideos() {
-  const videos = [
+  const videos = () => {
+    return <YouTube videoId="00TLrFv8ppI" className="w-[80%]" />;
+  };
+
+  const Images = [
+    {
+      renderItem: videos,
+      showPlayButton: false,
+      showFullscreenButton: false,
+      useBrowserFullscreen: false,
+      thumbnailWidth: 150,
+    },
     {
       original: "https://picsum.photos/id/1018/1000/600/",
+      showPlayButton: false,
+      showFullscreenButton: false,
+      useBrowserFullscreen: false,
+      sizes: "40px",
     },
     {
       original: "https://picsum.photos/id/1015/1000/600/",
+      showPlayButton: false,
+      showFullscreenButton: false,
+      useBrowserFullscreen: false,
     },
     {
       original: "https://picsum.photos/id/1019/1000/600/",
+      showPlayButton: false,
+      showFullscreenButton: false,
+      useBrowserFullscreen: false,
     },
   ];
-  return <ImageGallery items={videos} />;
-}
-
-/*
-<iframe
-  title="sample video"
-  width="560"
-  height="315"
-  src="https://www.youtube.com/embed/4pSzhZ76GdM?autoplay=1&amp;showinfo=0"
-  allowfullscreen=""
-  style="border: none;"
-></iframe>
-
-
-
-
-
-
-
-import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-//import "./styles.css";
-import { Navigation } from "swiper/modules";
-import dataMusician from "../../objects/musicianObject.json";
-import YouTube from "react-youtube";
-
-export default function CarouselVideos() {
-  const { users } = dataMusician;
 
   return (
     <>
-      <Swiper navigation={true} modules={[Navigation]}>
-        <SwiperSlide>
-          <YouTube videoId="kZKcHnQXksw"  />
-        </SwiperSlide>
-        <SwiperSlide>
-          <YouTube videoId="NsvDK26WvA0" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <YouTube videoId="d6RrGeLrgSk" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <YouTube videoId="VqN8X0RGp-c" />
-        </SwiperSlide>
-      </Swiper>
+      <ImageGallery items={Images} />
     </>
   );
-}*/
+}
