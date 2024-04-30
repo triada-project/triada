560d2dd264d621baffa328e6676a4e1076144c3e
Rodolfo Ramírez <126934315+RudyRmz@users.noreply.github.com>
2024-04-29 21:58:42 -0600

    Profile musician (#48)
    * create branch profileMusician, add in pages folder perfil-cliente
    
    * working in SelectGenreMusic component, done
    
    * the color of the Select chip was changed
    
    * working in InfoFormMusico
    
    * Working in MenuMobileMusician, layout done
    
    * working in AsideMusico Links, done
    
    * worked on conditional of AsideMusico from events, created svg component playCircleColor, done
    
    * worked on conditional of besidesMusico from events, created svg component playCircleColor
    
    * worked on conditional of MenuMobileMusician from events
    
    * saving some changues in AsideClient
    
    * MenuMobileMusician component added props with client and musician roles, finished
    
    * max-width and bg were applied to client-profile and musician-profile pages
    
    * Previous and next buttons were eliminated, flex end was applied to table pagination
    
    * size of the logo in MenuMobileMusician was increased from 50x44 to 66x50
    
    * selectType props are applied to SelectTypeEvents and SelectGenreMusic for home screen styles
    
    * EventsTable and data statuses were modified, svg icons were added according to the type of status
    
    * svg icons were changed as previous ones have the wrong size, status finished
    
    * anchor was added in more details
    
    * Work was done on the AsideMusico links to change styles according to the page
    
    * Work was done on the MenuMobileMusician links to change styles according to the page
    
    * Modal was added to MenuMobile and AsideMusico to log out
    
    * data folder with json was added for states and municipalities
    
    * StateSelect and LocationSelect were created, logical pending
    
    * Added states to EstadoSelect
    
    * Added states to EstadoSelect
    
    * Added else if to addRequest in requirements for limit of 5
    
    * Sonner dependency was added, toaster was integrated into requirements instead of the alert, it works
    
    * Added updateRequest function for requirements screen. In RequestCard useState was used to manage the state of the text to be edited, functionality finished
    
    * Zustand was implemented for state management. The stores folder was created, within it the store selectedStateStore was created, and it was implemented in the EstadoSelect and LocalidadSelect components
    
    * Styles are added to scroll. It is added to each profile page-musician overflow-y-auto
    
    * The scroll color was changed. Adjusted aside details to fixed top-0, replaced margin top
    
    * Event UI profile-musician ok
    
    * Responsive layout of availability, gallery, repertoire and requirements ok, pending details of availability, gallery and mobile menu
    
    * Toast was added if they send an empty request
    
    * Added inputs for eventCost and eventHours profile musician
    
    * React Hook Form was implemented to EstadoSelect component in InfoFormMusico, it works
    
    * React Hook Form was implemented to LocalidadSelect component in InfoFormMusico, it works
    
    * Controller was implemented to SelectTypeEvents and SelectGenreMusic components, onChangue and selectedKeys props are passed to these components, it works
    
    * NewPasswordForm component was created, it is added to the client-profile index, ok
    
    * NewPasswordForm component is added to the perfile-musico index, ok
    
    * InputPassword component was created and integrated into NewPasswordForm
    
    * Code was implemented in the client-profile InformationForm to obtain and decode the token
    
    * tokenStore was created. The token is saved in the store from the parent client-profile index component. Spinner was implemented in InformacionForm while loading the token
    
    * The PUT method in InformacionForm works, pending the defaultValue input being updated to update the name

--- components/perfil-cliente/InformacionForm.jsx
+++ components/perfil-cliente/InformacionForm.jsx
@@ -1,8 +1,10 @@
 import { Josefin_Sans, Lato } from "next/font/google";
 import { Input } from "@nextui-org/react";
-import { Select, SelectSection, SelectItem } from "@nextui-org/react";
+import { Spinner } from "@nextui-org/react";
 import ButtonPink from "./ButtonPink";
 import { useForm } from "react-hook-form";
+import useTokenStore from "@/stores/tokenStore";
+import { useEffect, useState } from "react";
 
 const josefine = Josefin_Sans({
   weight: ["300", "400", "600", "700"],
@@ -11,15 +13,43 @@ const josefine = Josefin_Sans({
 const lato = Lato({ weight: ["300", "400", "700"], subsets: ["latin"] });
 
 export default function InformacionForm() {
+  const tokenObject = useTokenStore((state) => state.tokenObject);
+
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
+  console.log(tokenObject);
+
   //console.log(errors);
 
-  const onSubmit = (data) => console.log(data);
+  //const onSubmit = (data) => console.log(data);
+
+  async function onSubmit(data) {
+    const response = await fetch(
+      `http://localhost:4000/users/${tokenObject?._id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: data.nombre,
+        }),
+      }
+    );
+  }
+
+  // Verificar si el tokenObject está listo
+  if (!tokenObject) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner label="Cargando..." color="secondary" labelColor="secondary" />
+      </div>
+    );
+  }
 
   return (
     <section className="flex flex-col items-center mt-11 lg:border lg:border-[#717171] lg:rounded lg:px-5 lg:py-5 lg:border-opacity-25 lg:shadow-lg lg:items-start lg:mt-[67px]">
@@ -38,43 +68,11 @@ export default function InformacionForm() {
             variant="bordered"
             radius="sm"
             label="Nombre para mostrar"
+            //placeholder={`${tokenObjet()?.name}`}
+            defaultValue={`${tokenObject?.name}`}
             {...register("nombre")}
           />
-          {/* <Select
-            label="Estado de residencia"
-            isRequired
-            //autoFocus={true}
-            variant="bordered"
-            radius="sm"
-            className={`w-[328px] h-14 lg:w-[30rem]`}
-            //errorMessage={!errors.estado ? "" : "Debes elegir un Estado"}
-            {...register("estado", { required: true })}
-          >
-            <SelectItem key={"Nuevo León"}>Nuevo León</SelectItem>
-            <SelectItem key={"CDMX"}>CDMX</SelectItem>
-            <SelectItem key={"Jalisco"}>Jalisco</SelectItem>
-          </Select> */}
-          {/* <div className=" text-tiny text-danger-50">
-            Debes elegir un estado
-          </div> */}
-          {/* <Select
-            label="Localidad"
-            isRequired
-            variant="bordered"
-            radius="sm"
-            className=" w-[328px] h-14 lg:w-[30rem]"
-            {...register("localidad")}
-          >
-            <SelectItem key={"Monterrey"}>Monterrey</SelectItem>
-            <SelectItem key={"Santa Catarina"}>Santa Catarina</SelectItem>
-            <SelectItem key={"San Pedro"}>San Pedro</SelectItem>
-          </Select> */}
-          {/* <Button
-            type="submit"
-            className={` bg-[#EF107D] text-white w-[328px] h-[50px] rounded text-base ${lato.className} lg:w-[30rem]`}
-          >
-            Guardar
-          </Button> */}
+
           <ButtonPink
             width="w-[328px] lg:w-[30rem]"
             text="Guardar"
