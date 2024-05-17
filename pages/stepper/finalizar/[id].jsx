0f56b687f02ce395807afffa9ef264f293f9d3f8
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-17 13:53:13 -0600

    se guardan cambios de componente disponibilidad

--- pages/stepper/finalizar/[id].jsx
+++ pages/stepper/finalizar/[id].jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { Select, SelectSection, SelectItem } from "@nextui-org/react";
 import SelectGenreMusic from "@/components/SelectGenreMusic/SelectGenreMusic";
 import { Input } from "@nextui-org/react";
+import { Spinner } from "@nextui-org/react";
 import ButtonPink from "@/components/perfil-cliente/ButtonPink";
 import SelectTypeEvents from "@/components/SelectGenreMusic/SelectTypeEvents";
 import useTokenStore from "@/stores/tokenStore";
@@ -24,15 +25,40 @@ export default function Finalizar() {
   const tokenObject = useTokenStore((state) => state.tokenObject);
   const userId = router.query.id;
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  useEffect(() => {
+    const tokenFromLocalStorage = localStorage.getItem("token");
+    if (tokenFromLocalStorage) {
+      const [encodedHeader, encodedPayload, encodedSignature] =
+        tokenFromLocalStorage.split(".");
+      const decodedPayload = atob(encodedPayload);
+      const payloadObject = JSON.parse(decodedPayload);
+      useTokenStore.setState({ tokenObject: payloadObject });
+    }
+  }, []);
 
-  //console.log(errors);
+  const handleFinalizar = () => {
+    // e.preventDefault();
+    setRoute(router.push("/perfil-musico"));
 
-  const onSubmit = (data) => console.log(data);
+    const response = fetch(`http://localhost:4000/users/${tokenObject?._id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id_stripe: userId,
+      }),
+    });
+  };
+
+  if (!tokenObject) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Spinner label="Cargando..." color="secondary" labelColor="secondary" />
+      </div>
+    );
+  }
+
   return (
     <StepperLayout>
       <section className=" w-[330px] mt-14 md:w-[404px] lg:w-[500px] flex flex-col items-center">
@@ -49,12 +75,17 @@ export default function Finalizar() {
           height={300}
           className=" sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]"
         />
-        <ButtonPink text="Finalizar" width="w-full" mtop="mt-[44px]" />
+        <ButtonPink
+          text="Finalizar"
+          width="w-full"
+          mtop="mt-[44px]"
+          onClick={handleFinalizar}
+        />
         {/* <ButtonsStepper
           mTop={"mt-[60px]"}
           step={"7"}
-          stepBack={"/stepper/paso6"}
-          stepNext={"/stepper/paso8"}
+          stepBack={`/stepper/finalizar/${userId}`}
+          stepNext={"/perfil-musico"}
         /> */}
       </section>
     </StepperLayout>
