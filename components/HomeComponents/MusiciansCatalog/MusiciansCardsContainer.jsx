6a99077bcf1c5408abf7036618b76a98a6e3eb0e
alejandro_junio29 <alejandro_junio29@hotmail.com>
2024-05-18 23:56:25 -0600

    musician cards random with verifications of imgProfile, address, & genre musical

--- components/HomeComponents/MusiciansCatalog/MusiciansCardsContainer.jsx
+++ components/HomeComponents/MusiciansCatalog/MusiciansCardsContainer.jsx
@@ -48,24 +48,50 @@ export default function MusiciansCardsContainer() {
     return () => window.removeEventListener("resize", updateNumCards);
   }, []);
 
+  // Function to shuffle an array using Fisher-Yates algorithm
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  // Shuffle musicians array each time before rendering
+  const shuffledMusicians = shuffleArray([...musicians]);
+
   return (
     <div
       id="bigContainer"
       className="bg-[#081540] flex flex-wrap justify-center sm:w-[744px] sm:justify-normal sm:grid sm:grid-cols-3 sm:gap-[11px] sm:px-[56px] lg:w-[1440px] lg:gap-[52px] lg:py-0 lg:px-[173px] "
     >
-      {musicians.slice(0, numCardsToShow).map((musician, index) => (
+      {shuffledMusicians.slice(0, numCardsToShow).map((musician, index) => (
         <div
           key={index}
           className={`flex justify-center w-full sm:justify-normal sm:pt-0 sm:w-1/2 p-4 lg:w-[324px] lg:p-0`}
         >
           <MusicianCard
             name={musician.name}
-            imgProfile={musician.imgProfile}
-            state={
-              musician.address.state + "," + " " + musician.address.country
+            imgProfile={
+              musician.imgProfile
+                ? musician.imgProfile
+                : "https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk=" // URL de la imagen por defecto
             }
-            chip1={musician.musicalGenre ? musician.musicalGenre[0] : ""} // Check if musicalGenre exists
-            chip2={musician.musicalGenre ? musician.musicalGenre[1] : ""} // Check if musicalGenre exists
+            state={
+              musician.address
+                ? `${musician.address.state}, ${musician.address.country}`
+                : "Locación no disponible"
+            }
+            chip1={
+              musician.musicalGenre
+                ? musician.musicalGenre[0]
+                : "Genero no disponible"
+            }
+            chip2={
+              musician.musicalGenre
+                ? musician.musicalGenre[1]
+                : "Genero no disponible"
+            }
           />
         </div>
       ))}
