e2a3be07332288360c39b8b343823ef8ed64942c
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-24 17:43:05 -0600

    galery hidden

--- components/profile-musician/AsideMusico.jsx
+++ components/profile-musician/AsideMusico.jsx
@@ -116,7 +116,7 @@ export default function AsideMusico({ page, hidden }) {
             </p>
           </Button>
         </Link>
-        <Link href="/perfil-musico/galeria">
+        {/*<Link href="/perfil-musico/galeria">
           <Button
             className={` rounded-none bg-[#081540] w-[245px] h-12 flex justify-start items-center gap-[18px] hover:bg-[#312971] pl-8 ${
               page === "galeria" ? "bg-[#312971]" : ""
@@ -131,7 +131,7 @@ export default function AsideMusico({ page, hidden }) {
               Galería
             </p>
           </Button>
-        </Link>
+            </Link>*/}
         <Link href="/perfil-musico/repertorio">
           <Button
             className={` rounded-none bg-[#081540] w-[245px] h-12 flex justify-start items-center gap-[18px] hover:bg-[#312971] pl-8 ${
