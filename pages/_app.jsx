64d2dcbd3affce66c479eb8daa2030967135f62e
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-05-31 13:10:26 -0600

    Se agrego icono a _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,18 @@
 import "@/styles/globals.css";
 import "@/styles/App_.css";
 import { NextUIProvider } from "@nextui-org/react";
+import triadaLogo from "../public/assets/images/TRIADA-LOGO.png";
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="../public/assets/images/TRIADA-LOGO.png"
+        />
+      </Head>
       <Component {...pageProps} />
     </NextUIProvider>
   );
