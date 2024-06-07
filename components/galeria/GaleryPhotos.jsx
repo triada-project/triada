9c58c1acd43ceb454630739543d20f3d346ff378
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2024-06-06 20:27:55 -0600

    Se comento codigo de componente galeria

--- components/galeria/GaleryPhotos.jsx
+++ components/galeria/GaleryPhotos.jsx
@@ -1,16 +1,16 @@
-import AddPhoto from "./AddPhoto";
-import CardPics from "./CardPics";
+// import AddPhoto from "./AddPhoto";
+// import CardPics from "./CardPics";
 
-export default function GalleryPhotos() {
-  return (
-    <>
-      <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
-        Fotos
-      </h2>
-      <div className="flex gap-4">
-        <AddPhoto />
-        <CardPics />
-      </div>
-    </>
-  );
-}
+// export default function GalleryPhotos() {
+//   return (
+//     <>
+//       <h2 className="{`${josefin.classname} text-[#37474F] font-semibold mt-5 sm:text-[20px]">
+//         Fotos
+//       </h2>
+//       <div className="flex gap-4">
+//         <AddPhoto />
+//         <CardPics />
+//       </div>
+//     </>
+//   );
+// }
